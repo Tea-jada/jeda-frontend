39de0a76f6f8b483cbc9e9b3ba78b3f4ad6879d1
kwj0605 <rladnwlsok@naver.com>
2025-07-06 23:50:58 +0900

    [feat] 푸터 개발 #1

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div>© 제다. All rights reserved.</div>
+      <div>광주광역시 충장로 224번길 123 | 대표전화 : 070-7777-7777</div>
+    </footer>
+  );
+}
+
+export default Footer;
