93750e298e044341b264d93d483f5274fe8cd56c
kwj0605 <rladnwlsok@naver.com>
2025-07-11 00:28:53 +0900

    [feat] 로그인 백엔드 연동 #1

--- src/api/user.js
+++ src/api/user.js
@@ -8,4 +8,16 @@ export async function signup({ email, username, password }) {
   });
   const data = await response.json();
   return { status: response.status, ...data };
+}
+
+export async function login({ username, password }) {
+  const response = await fetch(`${API_BASE_URL}/api/v1/users/login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password }),
+  });
+  const data = await response.json();
+  const authorization = response.headers.get('Authorization');
+  const refreshToken = response.headers.get('Refresh-Token');
+  return { status: response.status, ...data, authorization, refreshToken };
 } 
